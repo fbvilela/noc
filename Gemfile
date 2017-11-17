e5d97e293bc03332302b14b45c20f2279bda601d
Fabio Vilela <fbvilela@gmail.com>
2017-11-17 17:12:12 +1100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+
+
+source :rubygems
+
+gem 'sinatra'
+gem 'json'
+gem 'oauth2'
+gem 'tidyhqrb', path: '../tidyhqrb'
+gem 'faraday', '0.9.0'
---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'oauth2'
+require 'json'
+require 'tidyhqrb'
+
+
+enable :sessions
+
+CLIENT = '58de4c6c9173afea592bbd20b1fab08b94784e55c86b677572d079b0664b3bad'
+SECRET = '924b7709ad942c6b1e7fdeb039e79f7e67fe0fe45cb9f8927594551675beadfb'
+
+def client
+  client ||= OAuth2::Client.new(CLIENT, SECRET, {
+                :site => 'https://accounts.tidyhq.com',
+                :authorize_url => "/oauth/authorize",
+                :token_url => "/oauth/token"
+              })
+end
+
+get '/' do
+  erb :index
+end
+
+get '/test' do
+  @contacts = Tidyhqrb::Client.new.contacts.all(access_token: session[:access_token])
+  erb :test
+end
+
+get "/auth" do
+  redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri)
+end
+
+get '/callback' do
+  access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+  session[:access_token] = access_token.token
+  @message = "Successfully authenticated with the server"
+  @access_token = session[:access_token]
+
+  erb :success
+end
+
+def redirect_uri
+  uri = URI.parse(request.url)
+  uri.path = '/callback'
+  uri.query = nil
+  uri.to_s
+end
