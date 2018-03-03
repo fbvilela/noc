32e8dac7ccd942af157a97aed16facc8f2b4ef8a
Daniel Ribeiro <djbreves@gmail.com>
2018-03-03 11:26:45 +1100

    Remove unused actions

--- app.rb
+++ app.rb
@@ -40,81 +40,9 @@ class App < Sinatra::Base
     erb :success
   end
 
-  get '/contacts.json' do
-    content_type :json
-    tidyhq.contacts.all.to_json
-  end
-
-  get '/groups.json' do
-    content_type :json
-    tidyhq.groups.all.to_json
-  end
-
-  get '/groups/:id.json' do
-    content_type :json
-    tidyhq.groups.get(params['id'].to_i).to_json
-  end
-
-  get '/groups/:id/contacts.json' do
-    content_type :json
-    tidyhq.groups.get(params['id'].to_i).contacts.all.to_json
-  end
-
   get '/groups/:id/contacts' do
     @contacts = tidyhq.groups.get(params['id'].to_i).contacts.all
     erb :contacts
-  end
-
-  get '/memberships.json' do
-    content_type :json
-    tidyhq.memberships.all(params).to_json
-  end
-
-  get '/membership_levels.json' do
-    content_type :json
-    tidyhq.membership_levels.all.to_json
-  end
-
-  get '/membership_levels/:id/memberships.json' do
-    content_type :json
-    tidyhq.membership_levels.get(params[:id]).memberships.all.to_json
-  end
-
-  get '/orders.json' do
-    content_type :json
-    tidyhq.orders.all.to_json
-  end
-
-  get '/products.json' do
-    content_type :json
-    tidyhq.products.all.to_json
-  end
-
-  get '/products/:category_id.json' do
-    content_type :json
-    tidyhq.products.all.select do |product|
-      product.sell_category_id === params['category_id'].to_i
-    end.to_json
-  end
-
-  get '/products/:category_id/orders.json' do
-    content_type :json
-    products = tidyhq.products.all
-    orders = tidyhq.orders.all(created_since: params['created_since'])
-    orders.select do |order|
-      order.products.any? do |product_order|
-        product = products.find {|prod| prod.id === product_order.product_id }
-        product.sell_category_id === params['category_id'].to_i
-      end
-    end.to_json
-  end
-
-  get '/products/:category_id/orders/:type.csv' do
-    halt(404) unless params['type'] == 'list' || params['type'] == 'summary'
-
-    content_type 'application/csv'
-    attachment "orders-#{params['category_id']}-#{params['created_since']}.csv"
-    CsvGenerator.new(tidyhq).generate(params['category_id'].to_i, params['created_since'])[params['type'].to_sym]
   end
 
   private
